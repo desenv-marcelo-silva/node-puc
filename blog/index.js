020f0cd473a70dbf29bf80af728d062cb1665cf5
Marcelo Silva <desenv.marcelo.silva@gmail.com>
2017-07-03 21:49:11 -0300

    Novas rotas de login e logout. Exibição do nome de usuário e menus.

--- blog/index.js
+++ blog/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var urlEncodedParser = bodyParser.urlencoded({extended: false});
+
+app.set("usuario", {nome: ''});
 
 // roteamentos
 app.use('/public', express.static('./public'));
@@ -11,7 +15,25 @@ app.get('/', (req,res) => {
 });
 
 app.get('/artigos', (req, res) => {
-    res.render('artigos'); // já sabe que é dentro da pasta views
+    res.render('artigos', {usuario: app.get("usuario")}); // já sabe que é dentro da pasta views
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', {usuario: app.get("usuario")}); // já sabe que é dentro da pasta views
+});
+
+app.post('/login', urlEncodedParser, (req, res) => {
+    if (req.body.email == "marcelo@gmail.com" && req.body.senha == "teste") {
+        app.set("usuario", {nome: "Marcelo Silva"});
+        res.redirect('admin/artigos');
+    } else {
+        res.render('logininvalido', {usuario: app.get("usuario")});
+    }
+});
+
+app.get('/logout', (req, res) => {
+    app.set("usuario", {nome: ''});
+    res.redirect('/artigos');
 });
 
 app.listen(3000);
